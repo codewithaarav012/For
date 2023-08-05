b687a28b02077c69dcacca737ce7b9655b9bed75
codewithaarav012 <141054720+codewithaarav012@users.noreply.github.com>
2023-08-05 11:14:49 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+int main(){
+        printf("\n");    printf("\n");    printf("\n");    printf("\n");
+    printf("Hii");
+    cout<<"\n";
+    cout<<"Sir";
+    printf("\n");    printf("\n");
+    cout<<"Mera Naam Aarav Raj Hai";    printf("\n");
+    printf("Sir Ye Ham Coding Se Banaye Hai!");
+        printf("\n");    printf("\n");    printf("\n");
+        cout<<"Sir,\n Ye Coding language Ka naam C&C++ Hai!!!!!";
+            printf("\n");    printf("\n");    printf("\n");    printf("\n");    printf("\n");
+    return 0;
+}
